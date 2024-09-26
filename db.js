0b0e6e513a32e54e7a118ad1f54f35ad51769d1c
MariaSuarez2023 <suarezaraujomaal@uvm.edu.ve>
2024-09-25 23:36:17 -0400

    Commit 3

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://suarezaraujomaal:P5v2Ln32LRAugS9c@clustermaria.hkuol.mongodb.net/?retryWrites=true&w=majority&appName=ClusterMaria', {
+        await mongoose.connect('mongodb+srv://suarezaraujomaal:P5v2Ln32LRAugS9c@clustermaria.hkuol.mongodb.net/basedetodo?retryWrites=true&w=majority&appName=ClusterMaria', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
